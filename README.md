01bb3c4a9867d1f8cb47799762c9de44983b3116
prabodhw96 <prabodhwankhede1996@gmail.com>
2018-06-02 19:06:56 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# Terminal News
+## Overview
+<p>Terminal News is a CLI for techies who use terminal a lot. Get lots of news from all genre, thanks to <b>NewsAPI.org</b></p>
+
+## Requirements
+* Python 3.5
+* Colorama (Install using pip: ``pip install colorama``)
+
+## Usage
+* Generate NewsAPI.org API key and paste it in ``user-api.txt``.
+* Just run ``python news.py`` to see it work.
+
+<b>Note: </b> Use cmder if the colors don't show up in Command Prompt or Powershell. Get it [here](http://cmder.net/).
+
+Screenshots
+![](https://raw.githubusercontent.com/prabodhw96/Terminal-news/master/screenshot1.png) 
+<br>
+![](https://raw.githubusercontent.com/prabodhw96/Terminal-news/master/screenshot2.png)
---
+++ news.py
@@ -0,0 +1,180 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+import json
+from colorama import Fore, Style, Back
+
+cmds = {"international": ["BBC News", "CNN", "Daily Mail", "The Guardian UK", "The New York Times", "The Telegraph"],
+        "national": ["The Times of India", "The Hindu"],
+        "technology": ["Engadget", "Hacker News", "Techcrunch", "TechRadar", "The Next Web"],
+        "sports": ["BBC Sport", "ESPN", "ESPN Cric info", "Fox Sports", "TalkSport"],
+        "finance": ["Bloomberg", "Business Insider", "Financial Times", "The Wall Street Journal", "Time", "Fortune"],
+        "entertainment": ["BuzzFeed", "Entertainment Weekly", "MTV News", "MTV News UK"],
+        "science": ["New Scientist"],
+        "blog": ["Reddit r all"],
+        "adventure": ["National Geographic"],
+        "help": ["help", "exit","list-news","sources"],
+        "list-news": ["international", "national", "technology", "sports", "finance", "entertainment", "science", "blog", "adventure"]
+        }
+api_key = None
+
+def news(source):
+    print(" 1. Top News\n 2. Latest News\n 3. Most Popular News\n 4. Return")
+    loop = 1
+    while loop:
+        flag = int(input("[1/2/3/4] >_ "))
+        if flag == 1:
+            url = "https://newsapi.org/v1/articles?source=" + source + "&sortBy=top&apiKey=" + api_key
+            loop = 0
+        elif flag == 2:
+            url = "https://newsapi.org/v1/articles?source=" + source + "&sortBy=latest&apiKey=" + api_key
+            loop = 0
+        elif flag == 3:
+            url = "https://newsapi.org/v1/articles?source=" + source + "&sortBy=popular&apiKey=" + api_key
+            loop = 0
+        elif flag == 4:
+            loop = 0
+            return console()
+        else:
+            print("Enter a valid no.")
+    try:
+        with urlopen(url) as httpob:
+            decob = httpob.read().decode("utf-8")
+            jsonob = json.loads(decob)
+            news = jsonob["articles"]
+    except HTTPError as e:
+        print("Invalid API")
+        create_api_file(file_name)
+        return console()
+    draw_border()
+    for n in news:
+        print(Fore.RESET)
+        try:
+            print(Back.YELLOW + src2(jsonob["source"]) + Back.RESET)
+            print((Style.BRIGHT +"By: " + n["author"]) + Style.RESET_ALL)
+        except:
+        	print((Style.BRIGHT +"By: " + src2(jsonob["source"])) + Style.RESET_ALL)
+        try:
+            print(Back.RED + (Style.BRIGHT + n["title"] + Style.RESET_ALL)) 
+            print(Fore.BLUE + n["description"] + Fore.RESET)
+        except:
+            print(Fore.RED + "SOME ERROR OCCURED\n" + Fore.RESET)
+        print(Back.BLUE +(Style.BRIGHT + "url: "+ n["url"]) + Style.RESET_ALL + Back.RESET)
+        try:
+        	print(Fore.GREEN + "Published At: "+ n["publishedAt"] + Fore.RESET )
+        except:
+        	draw_border()
+        	continue
+        draw_border()
+
+def sour():
+    url = "https://newsapi.org/v1/sources?language=en"
+    try:
+        with urlopen(url) as httpob:
+            decob = httpob.read().decode("utf-8")
+            jsonob = json.loads(decob)
+            sources = jsonob["sources"]
+
+    except HTTPError as e:
+        print("Invalid API")
+        create_api_file(file_name)
+        return console()
+
+    draw_border()
+    key = 1
+    for s in sources:
+        print(Fore.RESET)
+        try:
+            print(key, end="")
+            print(". " + Back.BLUE + (Style.BRIGHT +s["name"] + Style.RESET_ALL) + Back.RESET)
+            key = key+1
+        except:
+            print(Fore.RED + "SOME ERROR OCCURED\n" + Fore.RESET)
+    draw_border()
+    print("Enter the index of any source if you want to know more about it.\nEnter -1 for returning back to main menu\n")
+    loop = True
+    while loop:
+        flag = input(">")
+        try:
+            if flag == '-1':
+                loop = False
+            elif int(flag)>0 or int(flag)<60:
+                s = sources[int(flag) -1]
+                print('\n')
+                print(Back.BLUE + (Style.BRIGHT +s["name"] + Style.RESET_ALL) + Back.RESET)
+                print("Description: " + Fore.BLUE + s["description"] + Fore.RESET)
+                print("Category: " + Fore.RED + s["category"] + Fore.RESET)
+                print("Url: " + Fore.GREEN + (Style.BRIGHT + s["url"]) + Fore.RESET)
+                print("\nEnter any other index. Enter -1 to exit\n")            
+        except:
+            print("Invalid Entry\n")
+
+def draw_border():
+    width = 80
+    print(Style.BRIGHT+"-" * width +Style.RESET_ALL+"\n")
+
+def src(n):
+    k = n.replace(" ", "-")
+    return k
+
+def src2(n):
+	k = n.replace("-"," ")
+	return k
+
+def create_api_file(file_name):
+    global api_key
+    api_key = input("Enter a valid API key: ")
+
+    with open(file_name, "w") as f:
+        f.write(api_key + '\n')
+    f.close()
+    print("The API key is: " + api_key)
+
+def get_api():
+    global api_key
+    global file_name 
+    file_name = "user-api.txt"
+
+    try:
+        f = open(file_name, "r") 
+        api_key = f.readline()
+        f.close()
+        print("The API key is: " + api_key)
+        print("Do you want to change the API key? [Y/N]: ")
+        flag = input()
+        if flag == 'Y':
+            create_api_file(file_name)
+    except FileNotFoundError:
+        create_api_file(file_name)
+    if api_key == None or len(api_key) == 0:
+        create_api_file(file_name)
+
+def console():
+    while True:
+        cmd = input("(topic) >_ ")
+        if (cmd not in cmds["list-news"] and cmd not in cmds["help"]):
+            print(Fore.RED + "WRONG COMMAND")
+            print(Fore.GREEN + "Try these COMMANDS" + Fore.RESET)
+            for c in cmds["help"]:
+                print("    " + c)
+        elif cmd == "help":
+            print(Fore.GREEN + "Try these COMMANDS" + Fore.RESET)
+            for c in cmds["help"]:
+                print("    " + c)
+        elif cmd == "list-news":
+            print(Fore.GREEN + "Find news about any of these topics" + Fore.RESET)
+            for l in cmds["list-news"]:
+                print("    " + l)
+        elif cmd == "exit":
+            print(Style.RESET_ALL)
+            exit()
+        elif cmd == "sources":
+            sour()
+        else:
+            for n in cmds[cmd]:
+                s = src(n)
+                news(s)
+
+if __name__ == "__main__":
+    print("\nPowered by NewsAPI.org\n")
+    get_api()
+    console()
